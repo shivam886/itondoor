50ab58552c02d882f20335e49e237f47fcedb1af
0126prashant <prashantsom75@gmail.com>
2026-05-22 23:40:28 +0530

    Fix TS error in About page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -108,7 +108,7 @@ const GalleryCard = ({ item, index }: { item: any, index: number }) => {
     
     const cardVariants = {
         offscreen: { y: 50, opacity: 0 },
-        onscreen: { y: 0, opacity: 1, transition: { type: "spring", bounce: 0.4, duration: 0.8, delay: index * 0.1 } }
+        onscreen: { y: 0, opacity: 1, transition: { type: "spring" as const, bounce: 0.4, duration: 0.8, delay: index * 0.1 } }
     };
 
     return (
